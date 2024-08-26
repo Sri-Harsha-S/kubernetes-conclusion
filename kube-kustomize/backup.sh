34160b2317de25ae3b227c20fa008ea3262b5066
Ubuntu <ubuntu@ip-172-31-9-156.eu-west-1.compute.internal>
2024-08-26 20:42:26 +0000

    successful kustomize version

--- kube-kustomize/backup.sh
+++ kube-kustomize/backup.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +"%Y%m%d%H%M")
 BACKUP_FILE="$BACKUP_DIR/etcd-backup-$TIMESTAMP.tar.gz"
 
 mkdir -p $BACKUP_DIR
-
+#this version is not saving any backupfile
 sudo k3s etcd snapshot save $BACKUP_FILE
 
 echo "Backup completed: $BACKUP_FILE"
