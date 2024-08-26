b937968c52ca43911a52b1839a98f1090c0d3bc0
Ubuntu <ubuntu@ip-172-31-9-156.eu-west-1.compute.internal>
2024-08-26 20:44:41 +0000

    added some helping files. History.txt is to create the environment to implment smoothly

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ TIMESTAMP=$(date +"%Y%m%d%H%M")
 BACKUP_FILE="$BACKUP_DIR/etcd-backup-$TIMESTAMP.db"
 
 mkdir -p $BACKUP_DIR
-
+#just a reference purpose
 sudo k3s etcd snapshot save $BACKUP_FILE
 
 echo "Backup completed: $BACKUP_FILE"
